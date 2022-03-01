91cfce3c56cdf151a35bfefddd92d7f8c96e8a59
valekutce <43315242+valekutce@users.noreply.github.com>
2022-03-01 18:36:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".toggle").on("click", function () {
+        $(".container").stop().addClass("active");
+      });
+      
+      $(".close").on("click", function () {
+        $(".container").stop().removeClass("active");
+      });
+      
+
+})
